a3071a241eb8b3e7d67f3abef6fc14ad6d4c0146
Daniel Jilg <winsmith@winsmith.de>
2017-08-13 15:04:50 +0200

    Create User

--- ablator/user_management/urls.py
+++ ablator/user_management/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.urls.base import reverse
+
 from . import views
 
 urlpatterns = [
+    url(r'create/$', views.UserCreate.as_view(), name='user-create'),
     url(r'$', views.UserList.as_view(), name='user-list'),
 ]
--- ablator/user_management/views.py
+++ ablator/user_management/views.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import User
+from django.urls.base import reverse_lazy
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 
 
 class UserList(ListView):
     model = User
+
+
+class UserCreate(CreateView):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email', 'is_staff']
+    success_url = reverse_lazy('user-list')
