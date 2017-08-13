1fa06de56732106b2aef53a086e80be60fab29a9
Daniel Jilg <winsmith@winsmith.de>
2017-08-13 15:17:21 +0200

    Edit and Delete Users

--- ablator/user_management/urls.py
+++ ablator/user_management/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'create/$', views.UserCreate.as_view(), name='user-create'),
+    url(r'update/(?P<pk>[^/]+)/$', views.UserUpdate.as_view(), name='user-update'),
+    url(r'delete/(?P<pk>[^/]+)/$', views.UserDelete.as_view(), name='user-delete'),
     url(r'$', views.UserList.as_view(), name='user-list'),
 ]
--- ablator/user_management/views.py
+++ ablator/user_management/views.py
@@ -1,14 +1,31 @@
 from django.contrib.auth.models import User
 from django.urls.base import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
+from django.contrib.admin.views.decorators import staff_member_required
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class UserList(ListView):
     model = User
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class UserCreate(CreateView):
     model = User
     fields = ['username', 'first_name', 'last_name', 'email', 'is_staff']
+    success_url = reverse_lazy('user-list')
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class UserUpdate(UpdateView):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email', 'is_staff']
+    success_url = reverse_lazy('user-list')
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class UserDelete(DeleteView):
+    model = User
     success_url = reverse_lazy('user-list')
