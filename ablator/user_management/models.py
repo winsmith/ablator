9cbe8b3a4d067d71fcbc72eadf8422b74790cffa
Daniel Jilg <winsmith@winsmith.de>
2017-08-19 10:00:15 +0200

    Add created_at fields to AblatorUser and Organization

--- ablator/user_management/models.py
+++ ablator/user_management/models.py
@@ -6,6 +6,7 @@ class Organization(models.Model):
     """Groups users and apps"""
     name = models.CharField(max_length=140)
     slug = models.SlugField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.slug
@@ -15,6 +16,7 @@ class AblatorUser(models.Model):
     """Extension of the regular Django User Model with Ablator-Specific addons"""
     user = models.OneToOneField(User)
     organization = models.ForeignKey(Organization)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username
