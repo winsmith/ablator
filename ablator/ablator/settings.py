df27b1b6d95ed0de7ee4f2a1c4fcbbef3df80078
Daniel Jilg <winsmith@winsmith.de>
2017-09-11 18:05:13 +0200

    Add VersioningMiddleware

--- ablator/ablator/settings.py
+++ ablator/ablator/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'web_admin.middleware.VersioningMiddleware',
 ]
 
 ROOT_URLCONF = 'ablator.urls'
