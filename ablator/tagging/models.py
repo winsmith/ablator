dd2f7123f2e27387e1ddd68037aa6f4ae06809a5
Daniel Jilg <winsmith@winsmith.de>
2018-08-02 22:19:13 +0200

    Have a new Tag model

--- ablator/tagging/models.py
+++ ablator/tagging/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from core.models import ClientUser
+from user_management.models import Organization
+
+
+class Tag(models.Model):
+    """
+    A trait, label, or group for a set of users.
+
+    Tags can be used to categorize and group ClientUser objects into sub groups.
+    """
+    name = models.SlugField()
+    user = models.ManyToManyField(ClientUser)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
