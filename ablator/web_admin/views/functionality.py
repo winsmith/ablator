584fc27969b3422f37614e86847702d569cb373a
Daniel Jilg <winsmith@winsmith.de>
2019-03-23 18:44:01 +0100

    Revamped navigation

--- ablator/web_admin/views/functionality.py
+++ ablator/web_admin/views/functionality.py
@@ -17,6 +17,7 @@ class FunctionalityDetail(TemplateView):
         context_data = super().get_context_data(**kwargs)
         context_data['functionality'] = Functionality.objects.filter(
             app__organization=self.request.user.ablatoruser.organization).get(id=pk)
+        context_data['app'] = context_data['functionality'].app
         return context_data
 
 
